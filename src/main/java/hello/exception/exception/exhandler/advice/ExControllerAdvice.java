829cf923c895f5d2de2bf70144ffcc669fef0660
mjh9401 <mjh9401@naver.com>
2023-06-11 14:56:46 +0900

    ExceptinoResolver로 API 예외처리 적용

---
+++ src/main/java/hello/exception/exception/exhandler/advice/ExControllerAdvice.java
@@ -0,0 +1,38 @@
+package hello.exception.exception.exhandler.advice;
+
+import hello.exception.exception.exception.UserException;
+import hello.exception.exception.exhandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "hello.exception.exception.api")
+public class ExControllerAdvice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResult illegalExHandler(IllegalArgumentException e){
+        log.error("[exceptionHandler] ex",e);
+        return new ErrorResult("BAD",e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e){
+        log.error("[exceptionHandler] ex",e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e){
+        log.error("[exceptionHandler] ex",e);
+        return new ErrorResult("EX","내부오류");
+    }
+
+
+}
